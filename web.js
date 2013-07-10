2f885473f8a0bf2fe376ad70ec9c4e3c1945af5a
Val <valiko@okilav.com>
2013-07-10 15:54:27 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var fs      = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Worldi 2!');
+//  var index_html = fs.readFileSync("index.html");
+    
+    response.send('Hello World 2!');
 });
+  
 
+
+  
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
