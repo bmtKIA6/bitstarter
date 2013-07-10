72008d99080731f0e5179dbed15af5a10b16eb3d
Val <valiko@okilav.com>
2013-07-10 15:55:51 +0000

    Updated web.js w/ index.html contents

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var fs      = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-//  var index_html = fs.readFileSync("index.html");
+    var index_html = fs.readFileSync("index.html");
     
-    response.send('Hello World 2!');
+    response.send(index_html);
 });
   
 
